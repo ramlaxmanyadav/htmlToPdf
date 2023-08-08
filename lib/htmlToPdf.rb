b0a1308decaa543b1e900e1d710116ac245c7826
ram <ram.yadav@reddoorz.com>
2023-08-08 13:19:49 +0530

    adding support for rails 7

--- lib/htmlToPdf.rb
+++ lib/htmlToPdf.rb
@@ -4,7 +4,6 @@ module HtmlToPdf
 
   def html_to_pdf()
     pdf_options = params[:pdf_options] ? params[:pdf_options] : {} 
-    _pdf_options = ""
     if request.format == 'application/pdf'
       initialize_downloads(pdf_options)
       FileUtils.mkdir_p(File.join(Rails.root, 'tmp'))
@@ -19,20 +18,12 @@ module HtmlToPdf
       _assigns.merge!({url: Rails.application.routes.url_helpers})
       
       _html = render_to_string(template: "#{controller_name}/#{action_name}", :layout => @layout, locals: _assigns)
-      if  File.exist?('../public/assets')
-        file_content = _html.gsub(/\/assets/, "../public/assets")
-      else
-        file_content = _html.gsub(/\/assets/, "../app/assets/stylesheets")
-      end
       file_content = _html.gsub(/\\&quot;/, "")
 
 
       File.open(_source_file, "w+") do |f|
         f.write(file_content)
       end
-      pdf_options.delete(:title)
-      pdf_options.delete(:layout)
-      # _pdf_options = pdf_options.each {|key, value| _pdf_options+"--#{key.to_s} #{value} "}
       system("wkhtmltopdf #{_source_file} #{_destination_file}")
       File.delete(_source_file) if File.exist?(_source_file)
       send_data File.open(_destination_file, "rb") { |f| f.read }, :disposition => 'attachment',:filename => "#{@name}.pdf"
